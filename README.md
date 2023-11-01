c1ac07229f6f427ba75f020d318e5beb3fe3aae1
Denis Belous <dbelous@entrypoint.lv>
2023-11-01 09:18:52 +0200

    GE-2: Released version 1.0.1

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # BelousLib.GuidExtension
-This library helps convert GUIDs to integers and vice versa
+This library helps convert **GUIDs** to **int/long/short** and vice versa
 
 ```csharp
 using BelousLib.GuidExtension;
 
-namespace Program
+namespace Example
 {
     public class Program
     {
@@ -15,7 +15,27 @@ namespace Program
 
             //Output a7010000-0000-0000-0000-000000000000
             Console.WriteLine(423.ToGuid());
+
+            //Output 6748db38b5fd40c18066c0a0f1733377
+            Console.WriteLine(new Guid("6748db38-b5fd-40c1-8066-c0a0f1733377").ToStringWithoutDashes());
         }
     }
 }
 ```
+<h1 align="center">Where can I use it?</h1>
+
+- For example, in a database, instead of using a **GUID** as the primary key, which occupies 16 bytes, you can use **int** or **long**, which occupy 4 and 8 bytes respectively. However, when displaying data to the user, you can convert **int/long** to a **GUID**. This means that you've implemented the logic with **GUIDs** and saved memory.
+  
+  ```csharp
+  cfg.CreateMap<User, UserDto>()
+    .ForMember(d => d.Id, o => o.MapFrom(s => s.Id.ToGuid()));
+  ```
+   ```csharp
+  cfg.CreateMap<UserDto, User>()
+    .ForMember(d => d.Id, o => o.MapFrom(s => s.Id.ToInt32()));
+  ```
+- If you don't want to display the identifiers of your entities, you can use this library as a form of **"encryption"** method.
+
+  **Before**: `/user?userId=1&teamId=59`
+  
+  **After**: `/user?userId=01000000-0000-0000-0000-000000000000&teamId=3b000000-0000-0000-0000-000000000000`
---
+++ src/BelousLib.GuidExtension/Extension.cs
@@ -0,0 +1,162 @@
+﻿namespace BelousLib.GuidExtension;
+
+/// <summary>
+///     Guid Extension
+/// </summary>
+public static class Extension
+{
+    private const int GuidLength = 32;
+
+    private const char Zero = '0';
+
+    private const char Dash = '-';
+
+    /// <summary>
+    ///     Convert GUID to Int16
+    /// </summary>
+    /// <param name="entityGuid">GUID</param>
+    public static short ToInt16(this Guid entityGuid)
+    {
+        return BitConverter.ToInt16(GetByteArrayFromHexString(entityGuid));
+    }
+
+    /// <summary>
+    ///     Convert GUID to Int32
+    /// </summary>
+    /// <param name="guid">GUID</param>
+    public static int ToInt32(this Guid entityGuid)
+    {
+        return BitConverter.ToInt32(GetByteArrayFromHexString(entityGuid));
+    }
+
+    /// <summary>
+    ///     Convert GUID to Int64
+    /// </summary>
+    /// <param name="entityGuid">GUID</param>
+    public static long ToInt64(this Guid entityGuid)
+    {
+        return BitConverter.ToInt64(GetByteArrayFromHexString(entityGuid));
+    }
+
+    /// <summary>
+    ///     Convert GUID to UInt16
+    /// </summary>
+    /// <param name="entityGuid">GUID</param>
+    public static ushort ToUInt16(this Guid entityGuid)
+    {
+        return BitConverter.ToUInt16(GetByteArrayFromHexString(entityGuid));
+    }
+
+    /// <summary>
+    ///     Convert GUID to UInt32
+    /// </summary>
+    /// <param name="entityGuid">GUID</param>
+    public static uint ToUInt32(this Guid entityGuid)
+    {
+        return BitConverter.ToUInt32(GetByteArrayFromHexString(entityGuid));
+    }
+
+    /// <summary>
+    ///     Convert GUID to UInt64
+    /// </summary>
+    /// <param name="entityGuid">GUID</param>
+    public static ulong ToUInt64(this Guid entityGuid)
+    {
+        return BitConverter.ToUInt64(GetByteArrayFromHexString(entityGuid));
+    }
+
+    /// <summary>
+    ///     Convert GUID to String without dashes
+    /// </summary>
+    /// <param name="entityGuid">GUID</param>
+    public static string ToStringWithoutDashes(this Guid entityGuid)
+    {
+        return entityGuid.ToString().Replace(Dash.ToString(), string.Empty, StringComparison.CurrentCulture);
+    }
+
+    /// <summary>
+    ///     Convert Int16 to GUID
+    /// </summary>
+    /// <param name="value">Int16</param>
+    public static Guid ToGuid(this short value)
+    {
+        return CreateGuid(Convert.ToHexString(BitConverter.GetBytes(value)));
+    }
+
+    /// <summary>
+    ///     Convert Int32 to GUID
+    /// </summary>
+    /// <param name="value">Int32</param>
+    public static Guid ToGuid(this int value)
+    {
+        return CreateGuid(Convert.ToHexString(BitConverter.GetBytes(value)));
+    }
+
+    /// <summary>
+    ///     Convert Int64 to GUID
+    /// </summary>
+    /// <param name="value">Int64</param>
+    public static Guid ToGuid(this long value)
+    {
+        return CreateGuid(Convert.ToHexString(BitConverter.GetBytes(value)));
+    }
+
+    /// <summary>
+    ///     Convert UInt16 to GUID
+    /// </summary>
+    /// <param name="value">UInt16</param>
+    public static Guid ToGuid(this ushort value)
+    {
+        return CreateGuid(Convert.ToHexString(BitConverter.GetBytes(value)));
+    }
+
+    /// <summary>
+    ///     Convert UInt32 to GUID
+    /// </summary>
+    /// <param name="value">UInt32</param>
+    public static Guid ToGuid(this uint value)
+    {
+        return CreateGuid(Convert.ToHexString(BitConverter.GetBytes(value)));
+    }
+
+    /// <summary>
+    ///     Convert UInt64 to GUID
+    /// </summary>
+    /// <param name="value">UInt64</param>
+    public static Guid ToGuid(this ulong value)
+    {
+        return CreateGuid(Convert.ToHexString(BitConverter.GetBytes(value)));
+    }
+
+    /// <summary>
+    ///     Create a new guid
+    /// </summary>
+    /// <param name="value">Numeric value in HEX</param>
+    /// <returns>
+    ///     New GUID
+    /// </returns>
+    private static Guid CreateGuid(string value)
+    {
+        var hexArray = value.ToCharArray();
+
+        var charArray = new char[GuidLength];
+
+        Array.Copy(hexArray, 0, charArray, 0, hexArray.Length);
+
+        for (var index = hexArray.Length; index < GuidLength; index++) charArray[index] = Zero;
+
+        return new Guid(new string(charArray));
+    }
+
+    /// <summary>
+    ///     Get Byte Array From Hex String
+    /// </summary>
+    /// <param name="guid">GUID</param>
+    /// <returns>
+    ///     Byte array
+    /// </returns>
+    private static byte[] GetByteArrayFromHexString(Guid guid)
+    {
+        return Convert.FromHexString(guid.ToString().Replace(Dash.ToString(), string.Empty, StringComparison.CurrentCulture));
+    }
+}
