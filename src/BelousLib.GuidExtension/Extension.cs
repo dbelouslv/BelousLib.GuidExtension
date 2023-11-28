6d1d6301d6a5b8a1e306c83535aaacdb8b01480c
Denis Belous <38912029+dbelouslv@users.noreply.github.com>
2023-11-28 22:56:40 +0200

    GE-6: Added nullable methods

--- src/BelousLib.GuidExtension/Extension.cs
+++ src/BelousLib.GuidExtension/Extension.cs
@@ -21,12 +21,30 @@ public static class Extension
     }
 
     /// <summary>
+    ///     Convert GUID? to Int16?
+    /// </summary>
+    /// <param name="entityGuid">GUID</param>
+    public static short? ToInt16(this Guid? entityGuid)
+    {
+        return entityGuid.HasValue ? BitConverter.ToInt16(entityGuid.Value.ToByteArray()) : null;
+    }
+
+    /// <summary>
     ///     Convert GUID to Int32
     /// </summary>
     /// <param name="entityGuid">GUID</param>
     public static int ToInt32(this Guid entityGuid)
     {
         return BitConverter.ToInt32(entityGuid.ToByteArray());
+    }
+
+    /// <summary>
+    ///     Convert GUID? to Int32?
+    /// </summary>
+    /// <param name="entityGuid">GUID</param>
+    public static int? ToInt32(this Guid? entityGuid)
+    {
+        return entityGuid.HasValue ? BitConverter.ToInt32(entityGuid.Value.ToByteArray()) : null;
     }
 
     /// <summary>
@@ -37,6 +55,15 @@ public static class Extension
     {
         return BitConverter.ToInt64(entityGuid.ToByteArray());
     }
+    
+    /// <summary>
+    ///     Convert GUID? to Int64?
+    /// </summary>
+    /// <param name="entityGuid">GUID</param>
+    public static long? ToInt64(this Guid? entityGuid)
+    {
+        return entityGuid.HasValue ? BitConverter.ToInt64(entityGuid.Value.ToByteArray()) : null;
+    }
 
     /// <summary>
     ///     Convert GUID to UInt16
@@ -45,6 +72,15 @@ public static class Extension
     public static ushort ToUInt16(this Guid entityGuid)
     {
         return BitConverter.ToUInt16(entityGuid.ToByteArray());
+    }
+    
+    /// <summary>
+    ///     Convert GUID? to UInt16?
+    /// </summary>
+    /// <param name="entityGuid">GUID</param>
+    public static ushort? ToUInt16(this Guid? entityGuid)
+    {
+        return entityGuid.HasValue ? BitConverter.ToUInt16(entityGuid.Value.ToByteArray()) : null;
     }
 
     /// <summary>
@@ -55,6 +91,15 @@ public static class Extension
     {
         return BitConverter.ToUInt32(entityGuid.ToByteArray());
     }
+    
+    /// <summary>
+    ///     Convert GUID? to UInt32?
+    /// </summary>
+    /// <param name="entityGuid">GUID</param>
+    public static uint? ToUInt32(this Guid? entityGuid)
+    {
+        return entityGuid.HasValue ? BitConverter.ToUInt32(entityGuid.Value.ToByteArray()) : null;
+    }
 
     /// <summary>
     ///     Convert GUID to UInt64
@@ -63,6 +108,15 @@ public static class Extension
     public static ulong ToUInt64(this Guid entityGuid)
     {
         return BitConverter.ToUInt64(entityGuid.ToByteArray());
+    }
+    
+    /// <summary>
+    ///     Convert GUID? to UInt64?
+    /// </summary>
+    /// <param name="entityGuid">GUID</param>
+    public static ulong? ToUInt64(this Guid? entityGuid)
+    {
+        return entityGuid.HasValue ? BitConverter.ToUInt64(entityGuid.Value.ToByteArray()) : null;
     }
 
     /// <summary>
@@ -73,6 +127,15 @@ public static class Extension
     {
         return BitConverter.ToSingle(entityGuid.ToByteArray());
     }
+    
+    /// <summary>
+    ///     Convert GUID? to Float?
+    /// </summary>
+    /// <param name="entityGuid">GUID</param>
+    public static float? ToSingle(this Guid? entityGuid)
+    {
+        return entityGuid.HasValue ? BitConverter.ToSingle(entityGuid.Value.ToByteArray()) : null;
+    }
 
     /// <summary>
     ///     Convert GUID to Double
@@ -81,6 +144,15 @@ public static class Extension
     public static double ToDouble(this Guid entityGuid)
     {
         return BitConverter.ToDouble(entityGuid.ToByteArray());
+    }
+    
+    /// <summary>
+    ///     Convert GUID? to Double?
+    /// </summary>
+    /// <param name="entityGuid">GUID</param>
+    public static double? ToDouble(this Guid? entityGuid)
+    {
+        return entityGuid.HasValue ? BitConverter.ToDouble(entityGuid.Value.ToByteArray()) : null;
     }
 
     /// <summary>
@@ -91,6 +163,15 @@ public static class Extension
     {
         return entityGuid.ToString();
     }
+    
+    /// <summary>
+    ///     Convert GUID? to String?
+    /// </summary>
+    /// <param name="entityGuid">GUID</param>
+    public static string? ToStringFromGuid(this Guid? entityGuid)
+    {
+        return entityGuid.HasValue ? entityGuid.Value.ToString() : null;
+    }
 
     /// <summary>
     ///     Convert GUID to String without dashes
@@ -99,6 +180,15 @@ public static class Extension
     public static string ToStringFromGuidWithoutDashes(this Guid entityGuid)
     {
         return entityGuid.ToString().Replace(Dash, string.Empty, StringComparison.CurrentCulture);
+    }
+    
+    /// <summary>
+    ///     Convert GUID? to String? without dashes
+    /// </summary>
+    /// <param name="entityGuid">GUID</param>
+    public static string? ToStringFromGuidWithoutDashes(this Guid? entityGuid)
+    {
+        return entityGuid.HasValue ? entityGuid.Value.ToString().Replace(Dash, string.Empty, StringComparison.CurrentCulture) : null;
     }
 
     /// <summary>
@@ -110,6 +200,16 @@ public static class Extension
     {
         return CreateGuid(BitConverter.GetBytes(value), enableZeroRemoving);
     }
+    
+    /// <summary>
+    ///     Convert Int16? to GUID?
+    /// </summary>
+    /// <param name="num">Int16</param>
+    /// <param name="enableZeroRemoving">Enable Zero Removing</param>
+    public static Guid? ToGuid(this short? num, bool enableZeroRemoving = false)
+    {
+        return num.HasValue ? CreateGuid(BitConverter.GetBytes(num.Value), enableZeroRemoving) : null;
+    }
 
     /// <summary>
     ///     Convert Int32 to GUID
@@ -119,6 +219,16 @@ public static class Extension
     public static Guid ToGuid(this int value, bool enableZeroRemoving = false)
     {
         return CreateGuid(BitConverter.GetBytes(value), enableZeroRemoving);
+    }
+    
+    /// <summary>
+    ///     Convert Int32? to GUID?
+    /// </summary>
+    /// <param name="num">Int32</param>
+    /// <param name="enableZeroRemoving">Enable Zero Removing</param>
+    public static Guid? ToGuid(this int? num, bool enableZeroRemoving = false)
+    {
+        return num.HasValue ? CreateGuid(BitConverter.GetBytes(num.Value), enableZeroRemoving) : null;
     }
 
     /// <summary>
@@ -130,6 +240,16 @@ public static class Extension
     {
         return CreateGuid(BitConverter.GetBytes(value), enableZeroRemoving);
     }
+    
+    /// <summary>
+    ///     Convert Int64? to GUID?
+    /// </summary>
+    /// <param name="num">Int64</param>
+    /// <param name="enableZeroRemoving">Enable Zero Removing</param>
+    public static Guid? ToGuid(this long? num, bool enableZeroRemoving = false)
+    {
+        return num.HasValue ? CreateGuid(BitConverter.GetBytes(num.Value), enableZeroRemoving) : null;
+    }
 
     /// <summary>
     ///     Convert UInt16 to GUID
@@ -139,6 +259,16 @@ public static class Extension
     public static Guid ToGuid(this ushort value, bool enableZeroRemoving = false)
     {
         return CreateGuid(BitConverter.GetBytes(value), enableZeroRemoving);
+    }
+    
+    /// <summary>
+    ///     Convert UInt16 to GUID
+    /// </summary>
+    /// <param name="num">UInt16</param>
+    /// <param name="enableZeroRemoving">Enable Zero Removing</param>
+    public static Guid? ToGuid(this ushort? num, bool enableZeroRemoving = false)
+    {
+        return num.HasValue ? CreateGuid(BitConverter.GetBytes(num.Value), enableZeroRemoving) : null;
     }
 
     /// <summary>
@@ -150,6 +280,16 @@ public static class Extension
     {
         return CreateGuid(BitConverter.GetBytes(value), enableZeroRemoving);
     }
+    
+    /// <summary>
+    ///     Convert UInt32? to GUID?
+    /// </summary>
+    /// <param name="num">UInt32</param>
+    /// <param name="enableZeroRemoving">Enable Zero Removing</param>
+    public static Guid? ToGuid(this uint? num, bool enableZeroRemoving = false)
+    {
+        return num.HasValue ? CreateGuid(BitConverter.GetBytes(num.Value), enableZeroRemoving) : null;
+    }
 
     /// <summary>
     ///     Convert UInt64 to GUID
@@ -159,6 +299,16 @@ public static class Extension
     public static Guid ToGuid(this ulong value, bool enableZeroRemoving = false)
     {
         return CreateGuid(BitConverter.GetBytes(value), enableZeroRemoving);
+    }
+    
+    /// <summary>
+    ///     Convert UInt64? to GUID?
+    /// </summary>
+    /// <param name="num">UInt64</param>
+    /// <param name="enableZeroRemoving">Enable Zero Removing</param>
+    public static Guid? ToGuid(this ulong? num, bool enableZeroRemoving = false)
+    {
+        return num.HasValue ? CreateGuid(BitConverter.GetBytes(num.Value), enableZeroRemoving) : null;
     }
 
     /// <summary>
@@ -170,6 +320,16 @@ public static class Extension
     {
         return CreateGuid(BitConverter.GetBytes(value), enableZeroRemoving);
     }
+    
+    /// <summary>
+    ///     Convert Float? to GUID?
+    /// </summary>
+    /// <param name="num">Float</param>
+    /// <param name="enableZeroRemoving">Enable Zero Removing</param>
+    public static Guid? ToGuid(this float? num, bool enableZeroRemoving = false)
+    {
+        return num.HasValue ? CreateGuid(BitConverter.GetBytes(num.Value), enableZeroRemoving) : null;
+    }
 
     /// <summary>
     ///     Convert Double to GUID
@@ -179,6 +339,16 @@ public static class Extension
     public static Guid ToGuid(this double value, bool enableZeroRemoving = false)
     {
         return CreateGuid(BitConverter.GetBytes(value), enableZeroRemoving);
+    }
+    
+    /// <summary>
+    ///     Convert Double? to GUID?
+    /// </summary>
+    /// <param name="num">Double</param>
+    /// <param name="enableZeroRemoving">Enable Zero Removing</param>
+    public static Guid? ToGuid(this double? num, bool enableZeroRemoving = false)
+    {
+        return num.HasValue ? CreateGuid(BitConverter.GetBytes(num.Value), enableZeroRemoving) : null;
     }
 
     /// <summary>
